9ef9a762b15f61608c335ffebf2c7a11d9eb05dd
Reggie Zhang <reggiezhang@users.noreply.github.com>
2016-09-27 16:52:12 +0800

    Update install-docker-compose.sh

--- install-docker-compose.sh
+++ install-docker-compose.sh
@@ -1,7 +1,7 @@
 sudo -i
 
 # Install docker-compose
-COMPOSE_VERSION=`git ls-remote https://github.com/docker/compose | grep refs/tags | grep -oP "[0-9]+\.[0-9]+\.[0-9]+$" | tail -n 1`
+COMPOSE_VERSION=`git ls-remote https://github.com/docker/compose | grep refs/tags | grep -oE "[0-9]+\.[0-9]+\.[0-9]+$" | tail -n 1`
 sudo sh -c "curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
 sudo chmod +x /usr/local/bin/docker-compose
 
